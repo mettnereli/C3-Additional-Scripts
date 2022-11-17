6c3b56cc143e6be93c3a4f2bdbf2ef605d3d5355
Elias Mettner <93096651+mettnereli@users.noreply.github.com>
2022-11-17 15:26:36 -0600

    updated for pex and psx histograms

--- programs/parentsHist.py
+++ programs/parentsHist.py
@@ -1,4 +1,4 @@
-#Correct method to access parentage info using awkward array, parquet file
+#Program to find parent particles of a geant4 C3 Simulation Elias Mettner, Lindsey Gray 
 import uproot
 import awkward
 import numpy as np
@@ -6,6 +6,7 @@ import os
 import math
 import hist
 
+#Recursive parent function
 def get_root_parent(index, mcparticles):
          parents = mcparticles[index]["parents"].tolist()
          if len(parents) == 0:
@@ -16,34 +17,79 @@ def get_root_parent(index, mcparticles):
                   raise Error("this cannot/should not happen")
 
 if __name__ == '__main__':
-         h1 = hist.Hist(hist.axis.Regular(bins=200, start=0, stop=45000, name="parent"))
-         h2 = hist.Hist(hist.axis.Regular(bins=100, start=-70, stop=70, name="z"),
-                        hist.axis.Regular(bins=100, start=0, stop=70, name="r"),
-                        )
-         h3 = hist.Hist(hist.axis.Regular(bins=50, start=0, stop=5, name="p"))
+         #h1 = hist.Hist(hist.axis.Regular(bins=50, start=0, stop=45000, name="parent"))
+         #h2 = hist.Hist(hist.axis.Regular(bins=50, start=-70, stop=70, name="z"),
+         #               hist.axis.Regular(bins=50, start=0, stop=70, name="r"),
+         #               )
+
+	 #pe momenta histograms
+         h3 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="pe"))
+         h4 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="peX"))
+         h5 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="peY"))
+         h6 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="peZ"))
+         
+	 #ps momenta histograms
+         h7 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="ps"))
+         h8 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="psX"))
+         h9 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="psY"))
+         h10 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="psZ"))
+
+	 #Open file/arrays
          x = awkward.from_parquet("C3Sid_extracted.parquet")
          mcParticles = x["MCParticles"]
          siVertexBarrelHits = x["SiVertexBarrelHits"]
+
+	 #iterate over each file
          for i in range(len(x)):
           print("Reading file " + str(i) + " . . . ")
-          for index in range(len(mcParticles[i])):
-                  parent = get_root_parent(index, mcParticles[i])
-                  h1.fill(parent=parent)
+
+          #for index in range(len(mcParticles[i])):
+                  #parent = get_root_parent(index, mcParticles[i])
+                  #h1.fill(parent=parent)
+
+	  #iterate over siVertexBarrelHits
           for index in range(len(siVertexBarrelHits[i])):
+
                   x = siVertexBarrelHits[i][index]["position"]["fCoordinates"]["fX"]
                   y = siVertexBarrelHits[i][index]["position"]["fCoordinates"]["fY"]
                   z = siVertexBarrelHits[i][index]["position"]["fCoordinates"]["fZ"]
-                  h2.fill(z=z, r=math.sqrt(x*x+y*y))
+                  #h2.fill(z=z, r=math.sqrt(x*x+y*y))
+		
+		  #Points at 0 position
                   if x == 0 and y == 0 and z == 0:
                    track = siVertexBarrelHits[i][index]["truth"]["trackID"]
                    parent = get_root_parent(track, mcParticles[i])
-                   pX = mcParticles[i][parent]["pex"]
-                   pY = mcParticles[i][parent]["pey"]
-                   pZ = mcParticles[i][parent]["pez"]
-                   p = math.sqrt(pX*pX+pY*pY+pZ*pZ)
-                   h3.fill(p=p)
+
+		   #Get peX and psX of parents
+                   peX = mcParticles[i][parent]["pex"]
+                   peY = mcParticles[i][parent]["pey"]
+                   peZ = mcParticles[i][parent]["pez"]
+                   psX = mcParticles[i][parent]["psx"]
+                   psY = mcParticles[i][parent]["psy"]
+                   psZ = mcParticles[i][parent]["psz"]
+                   pe = math.sqrt(peX*peX+peY*peY+peZ*peZ)
+                   ps = math.sqrt(psX*psX+psY*psY+psZ*psZ)
+
+		   #Fill
+                   h3.fill(pe=pe)
+                   h4.fill(peX=peX)
+                   h5.fill(peY=peY)
+                   h6.fill(peZ=peZ)
+                   h7.fill(ps=ps)
+                   h8.fill(psX=psX)
+                   h9.fill(psY=psY)
+                   h10.fill(psZ=psZ)
+
+
          print("Writing to file . . . ")
          f = uproot.recreate("parentPHists.root")
-         f["h1"] = h1
-         f["h2"] = h2
+         #f["h1"] = h1
+         #f["h2"] = h2
          f["h3"] = h3
+         f["h4"] = h4
+         f["h5"] = h5
+         f["h6"] = h6
+         f["h7"] = h7
+         f["h8"] = h8
+         f["h9"] = h9
+         f["h10"] = h10
