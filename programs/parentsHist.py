417dddd5f33d25463028f247cd7f2b33d5d1d10e
Elias Mettner <93096651+mettnereli@users.noreply.github.com>
2022-11-30 16:05:45 -0600

    Added p histograms for parentsHist

--- programs/parentsHist.py
+++ programs/parentsHist.py
@@ -22,17 +22,21 @@ if __name__ == '__main__':
          #               hist.axis.Regular(bins=50, start=0, stop=70, name="r"),
          #               )
 
-	 #pe momenta histograms
-         h3 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="pe"))
-         h4 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="peX"))
-         h5 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="peY"))
-         h6 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="peZ"))
+	 #pe parent momenta histograms
+         #h3 = hist.Hist(hist.axis.Regular(bins=20, start=0, stop=0.5, name="pe"))
+         #h4 = hist.Hist(hist.axis.Regular(bins=20, start=0, stop=0.5, name="peX"))
+         #h5 = hist.Hist(hist.axis.Regular(bins=20, start=0, stop=0.5, name="peY"))
+         #h6 = hist.Hist(hist.axis.Regular(bins=20, start=-100, stop=100, name="peZ"))
          
-	 #ps momenta histograms
-         h7 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="ps"))
-         h8 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="psX"))
-         h9 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="psY"))
-         h10 = hist.Hist(hist.axis.Regular(bins=40, start=0, stop=0.5, name="psZ"))
+	 #ps parent momenta histograms
+         h7 = hist.Hist(hist.axis.Regular(bins=80, start=-25000, stop=25000, name="psParent"))
+         h8 = hist.Hist(hist.axis.Regular(bins=80, start=-25000, stop=25000, name="psXParent"))
+         h9 = hist.Hist(hist.axis.Regular(bins=80, start=-25000, stop=25000, name="psYParent"))
+         h10 = hist.Hist(hist.axis.Regular(bins=80, start=-25000, stop=25000, name="psZParent"))
+         h11 = hist.Hist(hist.axis.Regular(bins=80, start=-25000, stop=25000, name="psHit"))
+         h12 = hist.Hist(hist.axis.Regular(bins=80, start=-25000, stop=25000, name="psXHit"))
+         h13 = hist.Hist(hist.axis.Regular(bins=80, start=-25000, stop=25000, name="psYHit"))
+         h14 = hist.Hist(hist.axis.Regular(bins=80, start=-25000, stop=25000, name="psZHit"))
 
 	 #Open file/arrays
          x = awkward.from_parquet("C3Sid_extracted.parquet")
@@ -67,29 +71,43 @@ if __name__ == '__main__':
                    psX = mcParticles[i][parent]["psx"]
                    psY = mcParticles[i][parent]["psy"]
                    psZ = mcParticles[i][parent]["psz"]
-                   pe = math.sqrt(peX*peX+peY*peY+peZ*peZ)
-                   ps = math.sqrt(psX*psX+psY*psY+psZ*psZ)
-
-		   #Fill
-                   h3.fill(pe=pe)
-                   h4.fill(peX=peX)
-                   h5.fill(peY=peY)
-                   h6.fill(peZ=peZ)
-                   h7.fill(ps=ps)
-                   h8.fill(psX=psX)
-                   h9.fill(psY=psY)
-                   h10.fill(psZ=psZ)
+                   pe = math.sqrt(peX*peX+peY*peY)
+                   ps = math.sqrt(psX*psX+psY*psY)
+		   
+                   #Get p of hits
+                   psXHit = mcParticles[i][track]["psx"]
+                   psYHit = mcParticles[i][track]["psy"]
+                   psZHit = mcParticles[i][track]["psz"]
+                   psHit = math.sqrt(psXHit**2 + psYHit**2)
+		  
+                   #Fill
+                   #h3.fill(pe=pe)
+                   #h4.fill(peX=peX)
+                   #h5.fill(peY=peY)
+                   #h6.fill(peZ=peZ)
+                   h7.fill(psParent=ps)
+                   h8.fill(psXParent=psX)
+                   h9.fill(psYParent=psY)
+                   h10.fill(psZParent=psZ)
+                   h11.fill(psHit=psHit)
+                   h12.fill(psXHit=psXHit)
+                   h13.fill(psYHit=psYHit)
+                   h14.fill(psZHit=psZHit)
 
 
          print("Writing to file . . . ")
-         f = uproot.recreate("parentPHists.root")
+         f = uproot.recreate("psZHist.root")
          #f["h1"] = h1
          #f["h2"] = h2
-         f["h3"] = h3
-         f["h4"] = h4
-         f["h5"] = h5
-         f["h6"] = h6
+         #f["h3"] = h3
+         #f["h4"] = h4
+         #f["h5"] = h5
+         #f["h6"] = h6
          f["h7"] = h7
          f["h8"] = h8
          f["h9"] = h9
          f["h10"] = h10
+         f["h11"] = h11
+         f["h12"] = h12
+         f["h13"] = h13
+         f["h14"] = h14
